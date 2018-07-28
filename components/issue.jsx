75f3e38b070793c2e9628ec9dfa8152a47b5a49c
Christopher Clark <fivefourths@gmail.com>
2018-07-27 19:14:03 -0700

    basic issue styles

--- components/issue.jsx
+++ components/issue.jsx
@@ -6,13 +6,34 @@ import IconCircleExclamation from './icon-circle-exclamation';
 const Issue = (props) => {
   return (
     <div {...props}>
-      <IconCircleExclamation/>
-      Hello World
+      <div>
+        <IssueName>ReferenceError</IssueName>
+        <span>/api/1/components</span>
+      </div>
+      <StyledIcon/>
     </div>
   )
 }
 const StyledTest = styled(Issue)`
-  color: red;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5em 0.5em 0.5em 1em;
+  background: #BD2215;
+  font-size: 1.2em;
+  font-family: sans-serif;
+  color: #fff;
+  border-radius: 2em;
+`
+
+const IssueName = styled('span')`
+  font-weight: bold;
+  margin-right: 0.5em;
+`
+
+const StyledIcon = styled(IconCircleExclamation)`
+  height: 2em;
+  width: 2em;
 `
 
 export default StyledTest;
