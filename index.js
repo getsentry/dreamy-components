3f4a61c7fbe8e05e4aa2c7b365ab60d574f0601e
Christopher Clark <fivefourths@gmail.com>
2018-09-06 11:43:00 -0700

    export stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+// illustrations
+
+export {default as Issues} from './components/issues';
+export {default as SuggestedAssignees} from './components/suggested-assignees';
+export {default as Resolution} from './components/resolution';
+export {default as Emails} from './components/emails';
+export {default as Contributors} from './components/contributors';
+
+// cards
+
+export {default as FilesCard} from './components/files-card';
+export {default as EmailCard} from './components/email-card';
+export {default as ContributorsCard} from './components/contributors-card';
+export {default as BashCard} from './components/bash-card';
+
+// basic components
+
+export {default as Issue} from './components/issue';
+export {default as CommitLine} from './components/commit-line';
+export {default as Card} from './components/card';
+export {default as Badge} from './components/badge';
+export {default as Avatar} from './components/avatar';
