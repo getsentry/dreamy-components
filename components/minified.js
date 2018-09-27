17908518494b4838498ed72ff65fef0170e8a925
Christopher Clark <fivefourths@gmail.com>
2018-09-27 15:16:55 -0700

    max height for minified blocks

--- components/minified.js
+++ components/minified.js
@@ -6,7 +6,9 @@ import theme from './theme';
 const Minified = () => (
   <BashCard header={<Header>sentry.min.js</Header>}>
     <MinifiedContent>
-      {`!function(){var n="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global==global&&global||this||{},r=n._,e=Array.prototype,o=Object.prototype,s="undefined"!=typeof Symbol?Symbol.prototype:null,u=e.push,c=e.slice,p=o.toString,i=o.hasOwnProperty;`}
+      <MinifiedBlock>
+        {`!function(){var n="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global==global&&global||this||{},r=n._,e=Array.prototype,o=Object.prototype,s="undefined"!=typeof Symbol?Symbol.prototype:null,u=e.push,c=e.slice,p=o.toString,i=o.hasOwnProperty;`}
+      </MinifiedBlock>
       <Callout>
         {'function() { if ('}
         <Orange>aProblem</Orange>
@@ -18,16 +20,23 @@ const Minified = () => (
         </Yellow>
         {' };'}
       </Callout>
-      {`f=function(){},h=function(n){return n instanceof h?n:this instanceof h?void(this._wrapped=n):new h(n)};"undefined"==typeof exports||exports.nodeType?n._=h:("undefined"!=typeof module&&!module.nodeType&&module.exports&&(exports=module.exports=h),exports._=h);`}
+      <MinifiedBlock>
+        {`f=function(){},h=function(n){return n instanceof h?n:this instanceof h?void(this._wrapped=n):new h(n)};"undefined"==typeof exports||exports.nodeType?n._=h:("undefined"!=typeof module&&!module.nodeType&&module.exports&&(exports=module.exports=h),exports._=h);`}
+      </MinifiedBlock>
     </MinifiedContent>
   </BashCard>
 );
 
 const MinifiedContent = styled('div')`
   padding: 2em 2em 2.25em 2em;
-  word-break: break-all;
   color: ${theme.purpleDark};
   font-weight: bold;
+`;
+
+const MinifiedBlock = styled('div')`
+  word-break: break-all;
+  max-height: 3.4em;
+  overflow: hidden;
 `;
 
 const Header = styled('span')`
