ade07bd011cb59aa1f66e77e6c0cb20f1fc56988
chrissy <christopherchristopherclark@gmail.com>
2019-01-02 14:40:27 -0800

    basic working version in all browsers using oldschool file-loader. performance issues in safari

--- stories/suggested-assignees.stories.js
+++ stories/suggested-assignees.stories.js
@@ -1,30 +1,10 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import styles from '../css/animations.css';
-import suggestedAssignees from '../svg/suggested-assignees.svg';
+import SuggestedAssignees from '../svg/suggested-assignees.svg';
 
 storiesOf('Suggested Assignees').add('default', () => ([
-  <svg
-    viewBox={suggestedAssignees.viewBox}
-    width={'20em'}
-    height={'20em'}
-  >
-    <use href={`#${suggestedAssignees.id}`} xlinkHref={`#${suggestedAssignees.id}`} />
-  </svg>,
-  <svg
-    viewBox={suggestedAssignees.viewBox}
-    width={'10em'}
-    height={'10em'}
-  >
-    <use href={`#${suggestedAssignees.id}`} xlinkHref={`#${suggestedAssignees.id}`} />
-  </svg>,
-  <svg
-    viewBox={suggestedAssignees.viewBox}
-    width={'5em'}
-    height={'5em'}
-  >
-    <use href={`#${suggestedAssignees.id}`} xlinkHref={`#${suggestedAssignees.id}`} />
-  </svg>
-]
-));
+  <img src={SuggestedAssignees} style={{width: "20em", height: "20em"}}/>,
+  <img src={SuggestedAssignees} style={{width: "10em", height: "10em", marginLeft: "6em"}}/>,
+  <img src={SuggestedAssignees} style={{width: "5em", height: "5em", marginLeft: "6em"}}/>,
+]));
