a429daa9d96c789467b13856d7061f60708c2542
chrissy <christopherchristopherclark@gmail.com>
2019-06-26 16:39:49 -0700

    more cleanup and upgrade storybook

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // illustrations
 
-module.exports.Issues = require('./dist/components/issues');
-module.exports.SuggestedAssignees = require('./dist/components/suggested-assignees');
-module.exports.Resolution = require('./dist/components/resolution');
-module.exports.Emails = require('./dist/components/emails');
-module.exports.Contributors = require('./dist/components/contributors');
-module.exports.Minified = require('./dist/components/minified');
+module.exports.Issues = require('./svg/components/issues');
+module.exports.SuggestedAssignees = require('./svg/components/suggested-assignees');
+module.exports.Resolution = require('./svg/components/resolution');
+module.exports.Emails = require('./svg/components/emails');
+module.exports.Contributors = require('./svg/components/contributors');
+module.exports.Minified = require('./svg/components/minified');
