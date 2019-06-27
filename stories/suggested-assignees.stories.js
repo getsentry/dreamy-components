3a66e991c4c47d09b1faf98f030e794d364066e3
chrissy <christopherchristopherclark@gmail.com>
2019-06-26 17:33:33 -0700

    early animation for suggested assignees

--- stories/suggested-assignees.stories.js
+++ stories/suggested-assignees.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
+import "../styles/styles.css";
 import suggestedAssignees from '../svg/suggested-assignees.svg';
 
 storiesOf('Suggested Assignees').add('default', () => (
