9c3b4294bc8612986c989a48a140d2dcf4e06e7d
chrissy <christopherchristopherclark@gmail.com>
2019-07-02 13:53:06 -0700

    remove index

--- index.js
+++
@@ -1,8 +0,0 @@
-// illustrations
-
-module.exports.Issues = require('./svg/components/issues');
-module.exports.SuggestedAssignees = require('./svg/components/suggested-assignees');
-module.exports.Resolution = require('./svg/components/resolution');
-module.exports.Emails = require('./svg/components/emails');
-module.exports.Contributors = require('./svg/components/contributors');
-module.exports.Minified = require('./svg/components/minified');
